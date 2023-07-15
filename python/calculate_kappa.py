e56ca323e8d05cf2a8f130c94574412ab0423968
Ulrich <ulrich.birkholz@sap.com>
2023-07-15 11:57:07 +0200

    Fix plot title Add boxplot description Add dual boxplots

--- python/calculate_kappa.py
+++ python/calculate_kappa.py
@@ -11,7 +11,7 @@ from collections import defaultdict
 from itertools import chain
 import os
 
-# TODO: step 2 combine multiple boxplots in one diagram using different colors
+
 class KappaFigure():
     def __init__(self, title:str, fileLabel:str) -> None:
         self.answer_count = -1
@@ -48,10 +48,6 @@ class KappaFigure():
                 self.answer_count = answer_count
 
 
-    def _add_figtext(self,figtext):
-        plt.figtext(0.5, 1, figtext, fontsize=12, bbox={'facecolor': 'grey', 'alpha': 0.5, 'pad': 10})
-
-
     def save(self, config: Configuration, figtext:str=None):
         figure = plt.figure()
         plt.title(self.title)
@@ -62,14 +58,21 @@ class KappaFigure():
         plt.plot(self.model_sample_sizes, self.kappa_values, 'ro')
 
         if figtext is not None:
-            self._add_figtext(figtext)
+            _add_figtext(figure, figtext)
         elif self.all_counts_equal is True:
-            self._add_figtext(f"Each value represents the agreement of exactly {self.answer_count} ratings")
+            _add_figtext(figure, f"Each value represents the agreement of exactly {self.answer_count} ratings")
         else:
-            self._add_figtext(f"Each value represents the agreement of at leased {self.answer_count} ratings")
+            _add_figtext(figure, f"Each value represents the agreement of at leased {self.answer_count} ratings")
 
         log.debug(f"Save Diagram: {self.filename}")
         figure.savefig(config.get_path_for_datafile(self.filename), bbox_inches='tight')
+
+
+def _add_figtext(fig, figtext):
+    fig.subplots_adjust(bottom=0.2)
+
+    fig.text(0.5, 0.1, '_'*80, ha='center')
+    fig.text(0.5, 0.08, figtext, ha='center', va='top')
 
 
 def _recursive_default_dict():
@@ -90,11 +93,14 @@ def _get_diagram_title(model_descriptor: str, answer_descriptor: str):
     }
 
     refined_type = "AI-Refined" if model_descriptor.endswith("ai") else "Expert-Refined"
+
+    refined_type = ("AI-Refined compared with Expert-Refined" if model_descriptor.endswith("ai_v_man") 
+                    else "AI-Refined" if model_descriptor.endswith("ai") 
+                    else "Expert-Refined")
+
     title = f"{platform} {refined_type}"
 
-    if model_descriptor.endswith("ai_v_man"):
-        title = f"{platform} AI-Refined compared with Expert-Refined ML Model\nrating "
-    elif answer_descriptor in descriptors:
+    if answer_descriptor in descriptors:
         title += descriptors[answer_descriptor]
     else:
         log.warning(f"The answer_descriptor {answer_descriptor} is invalid")
@@ -176,19 +182,88 @@ def _filter_model_ratings(modle_dict, model_set):
     
     return ratings, platform
 
-def _print_boxplot(identifier: str, x_y_boxplot_data):
+def _get_min_kappa_count(x_y_boxplot_data_sorted):
+    # y of 0 will not be rendered as box, y of 1 will reduce a box to a single line
+    # both will not just be visible within the diagram it also hardly qualifies as box so we ignore it
+    kappa_counts = [len(y) for x, y in x_y_boxplot_data_sorted.items() if len(y) > 1]
+    
+    if not kappa_counts:
+        return -1, True
+
+    min_kappa_count = min(kappa_counts)
+    all_equal = all(kappa == min_kappa_count for kappa in kappa_counts)
+
+    for x, y in x_y_boxplot_data_sorted.items():
+        log.debug(f"plotting box for sorted x:{x}; y:{y}")
+    
+    return min_kappa_count, all_equal
+
+
+def _print_dual_dataset_boxplot(identifier: str, x_y_model_data_1, x_y_model_data_2, title: str):
+    figure, ax = plt.subplots()
+
+    x_y_dataset_1 = x_y_model_data_1[0]
+    x_y_dataset_2 = x_y_model_data_2[0]
+    for key in set(x_y_dataset_1.keys()).union(x_y_dataset_2.keys()):
+        if key not in x_y_dataset_1:
+            x_y_dataset_1[key] = []
+        if key not in x_y_dataset_2:
+            x_y_dataset_2[key] = []
+
+    # sort x ascending
+    x_y_dataset_1_sorted = dict(sorted(x_y_dataset_1.items()))
+    x_y_dataset_2_sorted = dict(sorted(x_y_dataset_2.items()))
+
+    min_kappa_count_dataset_1, for_all_boxes_equal_d1 = _get_min_kappa_count(x_y_dataset_1_sorted)
+    min_kappa_count_dataset_2, for_all_boxes_equal_d2 = _get_min_kappa_count(x_y_dataset_2_sorted)
+
+    x_values_sorted = list(x_y_dataset_1_sorted.keys())
+
+    positions_1 = [i - 0.2 for i, key in enumerate(x_y_dataset_1_sorted.keys())]
+    positions_2 = [i + 0.2 for i, key in enumerate(x_y_dataset_1_sorted.keys())]
+
+    boxes_1 = ax.boxplot([sorted(data) for data in x_y_dataset_1_sorted.values()], positions=positions_1, widths=0.4, patch_artist=True)
+    boxes_2 = ax.boxplot([sorted(data) for data in x_y_dataset_2_sorted.values()], positions=positions_2, widths=0.4, patch_artist=True)
+
+    for bplot, color in zip((boxes_1, boxes_2), ["red", "blue"]):
+        for box in bplot['boxes']:
+            box.set_facecolor(color)
+
+    plt.xticks([i for i in range(len(x_values_sorted))], x_values_sorted)
+
+    if for_all_boxes_equal_d1 and for_all_boxes_equal_d2 and min_kappa_count_dataset_1 == min_kappa_count_dataset_2:
+        figtext = f"Each box represents exactly {min_kappa_count_dataset_1}"
+    else:
+        figtext = f"Each box represents at leased {min(min_kappa_count_dataset_1, min_kappa_count_dataset_2)}"
+    
+    figtext += f" kappa values\n{x_y_model_data_1[1]} is represented by red\n{x_y_model_data_2[1]} is represented by blue"
+    _add_figtext(figure, figtext)
+
+    ax.set_title(title)
+    ax.set_xlabel('Number of samples')
+    ax.set_ylabel('QWK')
+
+    figure.savefig(config.get_path_for_datafile(f"boxplot_kappa_{identifier}.pdf"), bbox_inches='tight')
+
+
+def _print_boxplot(identifier: str, x_y_boxplot_data, title: str):
     figure, ax = plt.subplots()
 
     # sort x ascending
     x_y_boxplot_data_sorted = dict(sorted(x_y_boxplot_data.items()))
 
-    for x, y in x_y_boxplot_data_sorted.items():
-        log.debug(f"plotting box for sorted x:{x}; y:{y}")
+    min_kappa_count, for_all_boxes_equal = _get_min_kappa_count(x_y_boxplot_data_sorted)
 
     # sort each individual y value ascending
     ax.boxplot([sorted(y) for y in x_y_boxplot_data_sorted.values()])
     ax.set_xticklabels(list(x_y_boxplot_data_sorted.keys()))
 
+    if for_all_boxes_equal:
+        _add_figtext(figure, f"Each box represents exactly {min_kappa_count} kappa values")
+    else:
+        _add_figtext(figure, f"Each box represents at leased {min_kappa_count} kappa values")
+
+    ax.set_title(title)
     ax.set_xlabel('Number of samples')
     ax.set_ylabel('QWK')
 
@@ -247,10 +322,9 @@ if __name__ == "__main__":
     score_1_v_2.save(config, f"The AI-Dataset consists of {len(all_ai_answers)} samples.\n The Expert-Dataset consists of {len(all_man_answers)} samples.")
     ########################
 
-    #
-    boxplots = _recursive_default_dict()
-
+    # [(ber|xgb)_(man|ai)][(ai|man)-(rating|training)][A-F]
     # [model_descriptor][answer_descriptor][id]
+    accumulated_data = _recursive_default_dict()
     for model_descriptor, answer_descriptor_dict in diagrams.items():
         for answer_descriptor, batch_id_dict in answer_descriptor_dict.items():
 
@@ -258,6 +332,7 @@ if __name__ == "__main__":
             x_y_boxplot_data = _recursive_default_dict()
             for batch_id, diagram in batch_id_dict.items():
 
+                #[50, .., 3200] [kappa_1, ... kappa_n]
                 d_x, d_y = diagram.get_values()
 
                 log.debug(f"Adding x: {d_x}, y: {d_y} to boxplot matrix")
@@ -271,4 +346,24 @@ if __name__ == "__main__":
 
                 diagram.save(config)
             
-            _print_boxplot(f"{model_descriptor}_{answer_descriptor}", x_y_boxplot_data)
+            title = _get_diagram_title(model_descriptor, answer_descriptor)
+            _print_boxplot(f"{model_descriptor}_{answer_descriptor}", x_y_boxplot_data, title)
+
+            if answer_descriptor in rating_data:
+                # [(ber|xgb)_(man|ai)][(ai|man)-(rating|training)] = (50 - 3200; A-F)
+                accumulated_data[model_descriptor][answer_descriptor] = x_y_boxplot_data
+
+    # boxplots with two datasets (ai vs man)
+    for model_set in model_sets:
+        for answer_descriptor in rating_data:
+            assert len(model_set) == 2, f"Inconsistent amount of model in set: {model_set}"
+            platform = _get_platform_from_model(model_set[0])
+
+            # We need to know which model belongs to which dataset
+            data_set_a = (accumulated_data[model_set[0]][answer_descriptor], model_set[0])
+            data_set_b = (accumulated_data[model_set[1]][answer_descriptor], model_set[1])
+
+            descriptor = f"{platform}_ai_vs_man"
+            # it is always ai_vs_man
+            title = _get_diagram_title(descriptor, answer_descriptor)
+            _print_dual_dataset_boxplot(f"{descriptor}_{answer_descriptor}", data_set_a, data_set_b, title)
